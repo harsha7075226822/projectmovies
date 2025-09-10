32b95c7f2a1b1118ed96112dc12052d75e654a28
harsha7075226822 <ugadiharshavardhan@gmail.com>
2025-09-10 15:55:49 +0530

    Updated all with social media icons

--- src/components/movieshomepage/index.jsx
+++ src/components/movieshomepage/index.jsx
@@ -1,6 +1,7 @@
 import TrendingMovies from '../TrendingMovies'
 import Originals from '../Originals'
 import MovieBanner from '../MovieBanner'
+import { FaGoogle, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 function MovieHomepage() {
 
@@ -11,6 +12,27 @@ function MovieHomepage() {
       <MovieBanner />
       <TrendingMovies />
       <Originals />
+      <div className="flex flex-col justify-center items-center bg-black p-5">
+        <div className='flex gap-3'>
+          {/* Google */}
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <FaGoogle className="text-red-500 text-3xl hover:text-red-700 transition duration-300" />
+          </a>
+          {/* Instagram */}
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-pink-500 text-3xl hover:text-pink-700 transition duration-300" />
+          </a>
+          {/* Twitter (X) */}
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-blue-500 text-3xl hover:text-blue-700 transition duration-300" />
+          </a>
+          {/* YouTube */}
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <FaYoutube className="text-red-600 text-3xl hover:text-red-800 transition duration-300" />
+          </a>
+        </div>
+        <p className='text-white mt-3'>Contact us</p>
+      </div>
     </div>
   )
 }
--- src/components/popularpage/index.jsx
+++ src/components/popularpage/index.jsx
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie'
 import { Navigate } from 'react-router'
 import Loading from '../Loading'
 import MovieItem from '../MovieItem'
+import { FaGoogle, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 function PopularPage() {
   const [popularMovies,setPopularMovies] = useState([])
@@ -51,6 +52,27 @@ function PopularPage() {
             </ul>
           </div>}
         </div>
+        <div className="flex flex-col justify-center items-center bg-black p-5">
+        <div className='flex gap-3'>
+          {/* Google */}
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <FaGoogle className="text-red-500 text-3xl hover:text-red-700 transition duration-300" />
+          </a>
+          {/* Instagram */}
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-pink-500 text-3xl hover:text-pink-700 transition duration-300" />
+          </a>
+          {/* Twitter (X) */}
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-blue-500 text-3xl hover:text-blue-700 transition duration-300" />
+          </a>
+          {/* YouTube */}
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <FaYoutube className="text-red-600 text-3xl hover:text-red-800 transition duration-300" />
+          </a>
+        </div>
+        <p className='text-white mt-3'>Contact us</p>
+      </div>
     </div>
   )
 }
